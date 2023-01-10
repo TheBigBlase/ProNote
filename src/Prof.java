d2f286f77def92f9a24038823e85c4dd3c168f9e
devinou971 <damiendasseux@gmail.com>
2023-01-10 15:11:55 +0100

    added constructor for prof

--- src/Prof.java
+++ src/Prof.java
@@ -2,6 +2,19 @@ package src;
 
 public class Prof {
 
-	private String nom;
+	public Prof(String nom, String prenom) {
+		this.nom = nom;
+		this.prenom = prenom;
+	}
 
+	private String nom;
+	private String prenom;
+
+	@Override
+	public String toString() {
+		return "Prof{" +
+				"nom='" + nom + '\'' +
+				", prenom='" + prenom + '\'' +
+				'}';
+	}
 }
