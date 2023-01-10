0801d3e173969546a779772d99f8a9dfb236526c
devinou971 <damiendasseux@gmail.com>
2023-01-10 14:42:40 +0100

    upload component

--- src/Component.java
+++ src/Component.java
@@ -1,10 +1,32 @@
 package src;
 
-public class Component {
+import java.util.ArrayList;
+import java.util.List;
 
-	public void getStudents() {
-		// TODO - implement Comppnent.getStudents
-		throw new UnsupportedOperationException();
+public class Component {
+	public ArrayList<Component> getChildren() {
+		return children;
 	}
 
+	private ArrayList<Component> children = new ArrayList<>();
+
+
+	public List<Eleve> getStudents() {
+		ArrayList<Eleve> eleves = new ArrayList<>();
+
+		if(this.children.size() == 0){
+			ArrayList<Eleve> el = new ArrayList<>();
+			el.add((Eleve) this);
+			return el;
+		}
+
+		for( Component c : this.children ){
+			eleves.addAll(c.getStudents());
+		}
+		return eleves;
+	}
+
+	public void addChild(Component component) {
+		this.children.add(component) ;
+	}
 }
